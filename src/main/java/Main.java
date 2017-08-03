f3a423d62d995efef159ce04fdbf19623192f001
LevStolpner <l.a.stolpner@gmail.com>
2017-08-04 01:38:30 +0300

    checking intersections between ships method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,17 @@
 package main.java;
 
-import main.java.model.Player;
+import main.java.model.Battlefield;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
 
-    public static int BATTLEFIELD_SIZE = 10;          //TODO complex figures for battlefields
-                                                        //TODO HEXAGONAL positions
+    private static int BATTLEFIELD_SIZE = 10;
 
     public static void main(String[] args) {
-        List<Player> players = new ArrayList<>(2);      //TODO games for more than 2?
-        players.add(new Player());
-        players.add(new Player());
-        initializeBattlefields(players);
-    }
-
-    public static void initializeBattlefields(List<Player> players) {
-        players.forEach(player -> player.initializeBattlefield(BATTLEFIELD_SIZE));
+        List<Battlefield> battlefields = new ArrayList<>(2);
+        battlefields.add(new Battlefield(BATTLEFIELD_SIZE));
+        battlefields.add(new Battlefield(BATTLEFIELD_SIZE));
     }
 }
--- src/main/java/model/Battlefield.java
+++ src/main/java/model/Battlefield.java
@@ -4,30 +4,52 @@ import java.util.List;
 
 public class Battlefield {
 
-    private Position battlefieldEnd;
+    private int battlefieldSize;
     private List<Ship> ships;
     private List<Position> shots;
 
     public Battlefield(int battlefieldSize) {
-        this.battlefieldEnd = new Position(battlefieldSize, battlefieldSize);
-    }
-
-    public void placeShip(Ship ship) {
-        checkShipPlacement(ship);
-
+        this.battlefieldSize = battlefieldSize;
     }
 
     //TODO check ship intersections
-    private boolean checkShipPlacement(Ship ship) {
+    public void placeShip(Ship ship) {
+        if (checkShipCoordinates(ship) && checkIntersectionsAndNearbyShips(ship)) {
+            ships.add(ship);
+        }
+    }
+
+    private boolean checkShipCoordinates(Ship ship) {
         Position pos = ship.getPosition();
-        if (pos.getX() < 0 || pos.getY() < 0) {
-            return false;
+        if (pos.getX() < 0 || pos.getY() < 0) return false;
+        return ship.getDirection() == Direction.Vertical ?
+                pos.getX() < battlefieldSize && pos.getY() + ship.getLength() <= battlefieldSize :
+                pos.getY() < battlefieldSize && pos.getX() + ship.getLength() <= battlefieldSize;
+    }
+
+    //TODO check nearby ships (more than 1 cell between ships)
+    private boolean checkIntersectionsAndNearbyShips(Ship newShip) {
+        for (Ship placedShip : ships) {
+            int placedX = placedShip.getPosition().getX();
+            int placedY = placedShip.getPosition().getY();
+            int newX = newShip.getPosition().getX();
+            int newY = newShip.getPosition().getY();
+
+            if (placedShip.getDirection() == newShip.getDirection()) {
+                if (newShip.getDirection() == Direction.Vertical && placedX == newX ||
+                        newShip.getDirection() == Direction.Horizontal && placedY == newY) {
+                    return false;
+                }
+            } else {
+                if ((placedShip.getDirection() == Direction.Vertical &&
+                        placedY <= newY && placedY + placedShip.getLength() >= newY &&
+                        placedX >= newX && placedX <= newX + newShip.getLength()) ||
+                    (placedShip.getDirection() == Direction.Horizontal &&
+                            placedX <= newX && placedX + placedShip.getLength() >= newX &&
+                            placedY >= newY && placedY <= newY + newShip.getLength()))
+                    return false;
+            }
         }
-        switch (ship.getDirection()) {
-            case Vertical:
-                return pos.getX() <= battlefieldEnd.getX() && pos.getY() + ship.getLength() - 1 <= battlefieldEnd.getY();
-            default:
-                return pos.getY() <= battlefieldEnd.getY() && pos.getX() + ship.getLength() - 1 <= battlefieldEnd.getX();
-        }
+        return true;
     }
 }

1fed1ab6db27719854f0c40752a8dd0ecbb37f14
LevStolpner <l.a.stolpner@gmail.com>
2017-08-05 01:38:34 +0300

    ship stock fixes

--- src/main/java/ShipStock.java
+++ src/main/java/ShipStock.java
@@ -5,14 +5,15 @@ import java.util.stream.Collectors;
 
 public class ShipStock {
 
-    private HashMap<Integer, List<Ship>> ships = new HashMap<>();
+    private HashMap<Integer, List<Ship>> ships;
 
     public ShipStock(HashMap<Integer, Integer> shipsData) {
-        shipsData.forEach((k, v) -> ships.put(k, new ArrayList<>(v).stream().map(s -> new Ship(k)).collect(Collectors.toList())));
+        this.ships = new HashMap<>();
+        shipsData.forEach((k, v) -> ships.put(k, Arrays.stream(new Integer[v]).map(s -> new Ship(k)).collect(Collectors.toList())));
     }
 
     public Ship takeShip(int length) {
-        return ships.containsKey(length) ? ships.get(length).remove(ships.size() - 1) : null;
+        return ships.containsKey(length) ? ships.get(length).remove(ships.get(length).size() - 1) : null;
     }
 
     //TODO make ships returnable

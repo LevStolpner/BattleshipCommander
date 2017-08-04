1f492d9c51d1951b591cd75a1a5011e179f1329b
LevStolpner <l.a.stolpner@gmail.com>
2017-08-05 01:29:21 +0300

    test data

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,9 +9,11 @@ public class Main {
     private static int BATTLEFIELD_SIZE = 10;
 
     public static void main(String[] args) {
-        List<Battlefield> battlefields = new ArrayList<>(2);
-        battlefields.add(new Battlefield(BATTLEFIELD_SIZE));
-        battlefields.add(new Battlefield(BATTLEFIELD_SIZE));
+        Battlefield field = new Battlefield(BATTLEFIELD_SIZE);
+        ShipStock stock = createShipStock();
+
+        //TODO start working on display (firstly show placed ships on one battlefield)
+        imitatePlacingShips(stock).forEach(field::placeShip);
     }
 
     public static ShipStock createShipStock() {
@@ -21,5 +23,61 @@ public class Main {
         ships.put(2, 3);
         ships.put(1, 4);
         return new ShipStock(ships);
+    }
+
+    public static List<Ship> imitatePlacingShips(ShipStock stock) {
+        List<Ship> result = new ArrayList<>();
+
+        result.add(stock.takeShip(4));
+        Ship lastShip = result.get(result.size() - 1);
+        lastShip.setPosition(new Position(3, 9));
+        lastShip.setDirection(Direction.Horizontal);
+
+        result.add(stock.takeShip(3));
+        lastShip = result.get(result.size() - 1);
+        lastShip.setPosition(new Position(7, 3));
+        lastShip.setDirection(Direction.Horizontal);
+
+        result.add(stock.takeShip(3));
+        lastShip = result.get(result.size() - 1);
+        lastShip.setPosition(new Position(3, 7));
+        lastShip.setDirection(Direction.Horizontal);
+
+        result.add(stock.takeShip(2));
+        lastShip = result.get(result.size() - 1);
+        lastShip.setPosition(new Position(4, 1));
+        lastShip.setDirection(Direction.Vertical);
+
+        result.add(stock.takeShip(2));
+        lastShip = result.get(result.size() - 1);
+        lastShip.setPosition(new Position(8, 1));
+        lastShip.setDirection(Direction.Horizontal);
+
+        result.add(stock.takeShip(2));
+        lastShip = result.get(result.size() - 1);
+        lastShip.setPosition(new Position(8, 7));
+        lastShip.setDirection(Direction.Vertical);
+
+        result.add(stock.takeShip(1));
+        lastShip = result.get(result.size() - 1);
+        lastShip.setPosition(new Position(6, 0));
+        lastShip.setDirection(Direction.Vertical);
+
+        result.add(stock.takeShip(1));
+        lastShip = result.get(result.size() - 1);
+        lastShip.setPosition(new Position(0, 4));
+        lastShip.setDirection(Direction.Vertical);
+
+        result.add(stock.takeShip(1));
+        lastShip = result.get(result.size() - 1);
+        lastShip.setPosition(new Position(1, 6));
+        lastShip.setDirection(Direction.Vertical);
+
+        result.add(stock.takeShip(1));
+        lastShip = result.get(result.size() - 1);
+        lastShip.setPosition(new Position(9, 5));
+        lastShip.setDirection(Direction.Vertical);
+
+        return result;
     }
 }

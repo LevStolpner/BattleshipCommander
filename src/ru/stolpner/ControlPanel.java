ce0ba7e0610158935b9689bf7029769d3fc98d24
LevStolpner <l.a.stolpner@gmail.com>
2017-08-26 20:42:12 +0300

    ship placing direction control

--- src/ru/stolpner/ControlPanel.java
+++ src/ru/stolpner/ControlPanel.java
@@ -3,17 +3,19 @@ package ru.stolpner;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.Toggle;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.input.MouseButton;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 
-import java.util.HashMap;
 import java.util.List;
 
 public class ControlPanel extends Parent {
 
-    private VBox ships = new VBox();
+    private VBox controls = new VBox();
 
     private int selectedLength = 0;
     private boolean isSelectedDirectionVertical = true;
@@ -33,9 +35,28 @@ public class ControlPanel extends Parent {
                 });
                 ship.getChildren().add(c);
             }
-            ships.getChildren().add(ship);
+            controls.getChildren().add(ship);
         }
-        getChildren().add(ships);
+
+        ToggleGroup group = new ToggleGroup();
+        group.selectedToggleProperty().addListener((ov, old_toggle, new_toggle) -> {
+            if (group.getSelectedToggle() != null) {
+                this.isSelectedDirectionVertical = (boolean) group.getSelectedToggle().getUserData();
+            }
+        });
+
+        RadioButton rb1 = new RadioButton("Vertical");
+        rb1.setToggleGroup(group);
+        rb1.setUserData(true);
+        rb1.setSelected(true);
+        RadioButton rb2 = new RadioButton("Horizontal");
+        rb2.setToggleGroup(group);
+        rb2.setUserData(false);
+
+        controls.getChildren().add(rb1);
+        controls.getChildren().add(rb2);
+        getChildren().add(controls);
+
         selectShipLength(4);
     }
 
@@ -52,7 +73,7 @@ public class ControlPanel extends Parent {
         if (this.selectedLength != 0 ) clearSelection();
 
         this.selectedLength = shipLength;
-        List<Node> selectedShipNodes = ((HBox) ships.getChildren().get(4 - selectedLength)).getChildren();
+        List<Node> selectedShipNodes = ((HBox) controls.getChildren().get(4 - selectedLength)).getChildren();
         for (int i = 0; i < shipLength; i++) {
             ControlPanelCell c = (ControlPanelCell) selectedShipNodes.get(i + 1);      //cause Text is 0 element in row
             c.colorSelected();
@@ -60,7 +81,7 @@ public class ControlPanel extends Parent {
     }
 
     private void clearSelection() {
-        List<Node> selectedShipNodes = ((HBox) ships.getChildren().get(4 - selectedLength)).getChildren();
+        List<Node> selectedShipNodes = ((HBox) controls.getChildren().get(4 - selectedLength)).getChildren();
         for (int i = 0; i < selectedLength; i++) {
             ControlPanelCell c = (ControlPanelCell) selectedShipNodes.get(i + 1);      //cause Text is 0 element in row
             c.resetColors();
